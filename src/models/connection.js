b872cdec04bb910cc3847f0e98949fd1b8dfc828
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-13 16:49:14 -0300

    fix: connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   port: process.env.MYSQL_PORT,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  database: 'StoreManager',
 });
 
 module.exports = connection;
