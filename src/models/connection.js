25974e097dcf10752c1f0320baf049c3fbf0d2b9
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-13 17:08:06 -0300

    fix: connection to database

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,21 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv/config');
+
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PORT,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env;
+
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  port: process.env.MYSQL_PORT,
-  password: process.env.MYSQL_PASSWORD,
-  database: 'StoreManager',
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  port: MYSQL_PORT,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
 });
 
 module.exports = connection;
