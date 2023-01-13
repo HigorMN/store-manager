88cdaee637668336dd429181705d5edb0760625b
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-13 19:42:20 -0300

    test: products service

---
+++ tests/unit/services/mocks/products.service.mock.js
@@ -0,0 +1,13 @@
+const invalidValue = 'a'
+
+const allProducts = [
+  {
+    "id": 1,
+    "name": "Martelo de Thor"
+  },
+];
+
+module.exports = {
+  invalidValue,
+  allProducts,
+};
---
+++ tests/unit/services/products.service.test.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { productsModel } = require('../../../src/models');
+const { productsService: { findAll, findById } } = require('../../../src/services/');
+const { allProducts, invalidValue } = require('./mocks/products.service.mock');
+
+describe('Testes service de produtos', () => {
+  describe('listagem de produtos', () => {
+    it('lista todos os produtos', async () => {
+      sinon.stub(productsModel, 'findAll').resolves(allProducts);
+  
+      const result = await findAll();
+  
+      expect(result.message).to.deep.equal(allProducts);
+    });
+  }); -
+
+  describe('busca 1 produto', () => {
+    it('retorna o produto caso id exista', async () => {
+      sinon.stub(productsModel, 'findById').resolves(allProducts[0]);
+
+      const result = await findById(1);
+
+      expect(result.type).to.equal(null);
+      expect(result.message).to.deep.equal(allProducts[0]);
+    });
+
+    it('retorna um erro caso o produto não existe', async () => {
+      sinon.stub(productsModel, 'findById').resolves(undefined);
+
+      const result = await findById(99999);
+
+      expect(result.type).to.equal('PRODUCT_NOT_FOUND');
+      expect(result.message).to.equal('Product not found');
+    })
+
+    it('retorna um erro caso o id seja inválido', async () => {
+      const result = await findById(invalidValue);
+
+      expect(result.type).to.equal('INVALID_VALUE');
+      expect(result.message).to.equal('"id" must be a number');
+    });
+  });
+
+  afterEach(function () { sinon.restore() });
+});
