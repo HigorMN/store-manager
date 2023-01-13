c0838f8f7725af5b85f2235902632d93029a74b7
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-13 19:44:35 -0300

    test: model products

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -5,7 +5,7 @@ const { productsModel } = require('../../../src/models');
 const connection = require('../../../src/models/connection');
 const { products } = require('./mocks/products.model.mock');
 
-describe('Testes de unidade do model de produtos', () => {
+describe('Testando model de produtos', () => {
   it('Lista todos os produtos', async () => {
     sinon.stub(connection, 'execute').resolves([products]);
     const result = await productsModel.findAll();
