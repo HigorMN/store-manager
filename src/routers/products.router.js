9266c1140a366c71a15674d530264356151df673
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-14 11:44:25 -0300

    feat: middleware validateName

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const validateName = require('../middlewares/validateName');
 
 const router = express.Router();
 
 router.get('/', productsController.listProducts);
 router.get('/:id', productsController.getProfuct);
-router.post('/', productsController.newProduct);
+router.post('/', validateName, productsController.newProduct);
 
 module.exports = router;
