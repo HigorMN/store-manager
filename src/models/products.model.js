008b3dba0f9821a61d446f51580246610e0cde5b
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-14 11:33:26 -0300

    feat: inserir produto model

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -13,7 +13,17 @@ const findById = async (id) => {
   return result;
 };
 
+const insert = async (product) => {
+  const [{ insertId }] = await connection.execute(
+    'INSERT INTO products (name) VALUE (?)',
+    [product],
+  );
+
+  return insertId;
+};
+
 module.exports = {
   findAll,
   findById,
+  insert,
 };
