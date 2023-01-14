5557439696e7749a463023cb78f33f0a9ee95e28
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-14 20:57:58 -0300

    feat: inserir novas vendas model

---
+++ src/models/sales.model.js
@@ -0,0 +1,25 @@
+const connection = require('./connection');
+
+const insert = async (arraySales) => {
+  await connection.execute('INSERT INTO sales (date) VALUE (NOW())');
+
+  const [[{ saleId }]] = await connection.execute(
+    'SELECT MAX(sale_id) AS saleId FROM sales_products',
+);
+  const id = +saleId + 1;
+
+  const sales = await Promise.all(arraySales.map(async (e) => {
+    const { productId, quantity } = e;
+    await connection.execute(
+      'INSERT INTO sales_products (sale_id, product_id, quantity) VALUE (?, ?, ?)',
+      [id, productId, quantity],
+    );
+      return e;
+  }));
+
+  return { id, itemsSold: sales };
+};
+
+module.exports = {
+  insert,
+};
