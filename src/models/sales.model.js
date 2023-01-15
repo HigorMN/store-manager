a1244c3b5dc9ced0661aef0ec314aa3f17376154
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-15 17:57:48 -0300

    feat: sales findAll and findById

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,3 +1,4 @@
+const camelize = require('camelize');
 const connection = require('./connection');
 
 const insert = async (arraySales) => {
@@ -15,6 +16,24 @@ const insert = async (arraySales) => {
   return { id, itemsSold: sales };
 };
 
+const findAll = async () => {
+  const query = `SELECT (sP.sale_id), (s.date), (sP.product_id), (sP.quantity) 
+  FROM sales_products AS sP INNER JOIN sales AS s ON s.id = sP.sale_id`;
+  const [result] = await connection.execute(query);
+
+  return camelize(result);
+};
+
+const findById = async (id) => {
+  const query = `SELECT (s.date), (sP.product_id), (sP.quantity)
+  FROM sales_products AS sP INNER JOIN sales AS s ON s.id = sP.sale_id WHERE sale_id = ?`;
+  const [result] = await connection.execute(query, [id]);
+
+  return camelize(result);
+};
+
 module.exports = {
   insert,
+  findAll,
+  findById,
 };
