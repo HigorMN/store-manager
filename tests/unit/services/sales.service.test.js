ced6ffb76f2d347f01a9d676738c7418dace2c53
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-15 19:29:45 -0300

    test: sales service find by id e find all

--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -4,9 +4,9 @@ const sinon = require('sinon');
 const { salesModel, productsModel } = require('../../../src/models');
 
 const { salesService } = require('../../../src/services');
-const { salesMock, insertMock } = require('./mocks/sales.service.mock')
+const { salesMock, insertMock, findAllMock, findByIdMock } = require('./mocks/sales.service.mock')
 describe('Testes de unidade do service de sales', () => {
-  it('Inserindo sales em caso de sucesso', async () => {
+  it('Inserindo vendas em caso de sucesso', async () => {
     sinon.stub(salesModel, 'insert').resolves(salesMock);
 
     const result = await salesService.insert(insertMock);
@@ -15,7 +15,7 @@ describe('Testes de unidade do service de sales', () => {
     expect(result.message).to.deep.equal(salesMock);
   });
 
-  it('Inserindo sales em caso de erro', async () => {
+  it('Inserindo vendas em caso de erro', async () => {
     sinon.stub(productsModel, 'findById').resolves(undefined);
 
     const salesInsert = await salesService.insert([insertMock[0]])
@@ -23,6 +23,40 @@ describe('Testes de unidade do service de sales', () => {
     expect(salesInsert.type).to.equal('PRODUCT_NOT_FOUND');
     expect(salesInsert.message).to.equal('Product not found');
   });
+
+  it('buscando todas as vendas', async () => {
+    sinon.stub(salesModel, 'findAll').resolves(findAllMock);
+
+    const result = await salesService.findAll();
+
+    expect(result.type).to.equal(null);
+    expect(result.message).to.deep.equal(findAllMock);
+  });
+
+  it('buscando venda pelo id valido', async () => {
+    sinon.stub(salesModel, 'findById').resolves(findByIdMock);
+
+    const result = await salesService.findById(1);
+
+    expect(result.type).to.equal(null);
+    expect(result.message).to.deep.equal(findByIdMock);
+  });
+
+  it('buscando venda pelo id inexistente', async () => {
+    sinon.stub(salesModel, 'findById').resolves([]);
+
+    const result = await salesService.findById(9999);
+    expect(result.type).to.equal('SALE_NOT_FOUND');
+    expect(result.message).to.deep.equal('Sale not found');
+  });
+
+  it('buscando venda pelo id invalido', async () => {
+    sinon.stub(salesModel, 'findById').resolves(undefined);
+
+    const result = await salesService.findById('a');
+    expect(result.type).to.equal('INVALID_VALUE');
+    expect(result.message).to.deep.equal('"id" must be a number');
+  })
 
   afterEach(function () { sinon.restore() });
 });
