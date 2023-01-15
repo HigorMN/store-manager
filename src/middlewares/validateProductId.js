2131199bca6bfb54d4150871ea1d77920c01a7d7
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-14 21:52:21 -0300

    fix: validateProfuct

--- src/middlewares/validateProductId.js
+++ src/middlewares/validateProductId.js
@@ -1,10 +1,12 @@
 module.exports = (req, res, next) => {
-  const sales = req.body;
-
-  sales.map(({ productId }) => {
+  const sales = req.body.map(({ productId }) => {
     if (!productId) {
       return res.status(400).json({ message: '"productId" is required' });
     }
-    return next();
+    return true;
   });
+
+  if (sales.every((e) => e === true)) {
+   return next(); 
+  }
 };
