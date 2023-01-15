064ad19797fc30577bc846d925ba544ee52cbb81
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-15 15:09:41 -0300

    refactor: sales model

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,13 +1,7 @@
 const connection = require('./connection');
 
 const insert = async (arraySales) => {
-  await connection.execute('INSERT INTO sales (date) VALUE (NOW())');
-
-  const [[{ saleId }]] = await connection.execute(
-    'SELECT MAX(sale_id) AS saleId FROM sales_products',
-  );
-  
-  const id = +saleId + 1;
+  const [{ insertId: id }] = await connection.execute('INSERT INTO sales (date) VALUE (NOW())');
 
   const sales = await Promise.all(arraySales.map(async (e) => {
     const { productId, quantity } = e;
