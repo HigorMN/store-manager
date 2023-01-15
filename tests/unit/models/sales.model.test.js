0d0c5d2b049ac17f14a13f4154fa0e203d3e533f
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-15 18:13:29 -0300

    test: buscando tudo e pelo id model

--- tests/unit/models/sales.model.test.js
+++ tests/unit/models/sales.model.test.js
@@ -3,7 +3,7 @@ const sinon = require("sinon");
 const { salesModel } = require("../../../src/models");
 const connection = require('../../../src/models/connection');
 
-const { salesMock, insertMock } = require("./mocks/sales.model.mock");
+const { salesMock, insertMock, findAllMock, findByIdMock } = require("./mocks/sales.model.mock");
 
 describe('Teste de unidade de Sales', () => {
   it('Iserir novas sales', async () => {
@@ -13,5 +13,22 @@ describe('Teste de unidade de Sales', () => {
 
     expect(result).to.be.deep.equal(salesMock);
   });
+
+  it('Buscando todas as sales', async () => {
+    sinon.stub(connection, 'execute').resolves([findAllMock]);
+
+    const result = await salesModel.findAll();
+
+    expect(result).to.be.deep.equal(findAllMock);
+  });
+
+  it('Buscando sales pelo id', async () => {
+    sinon.stub(connection, 'execute').resolves([findByIdMock]);
+
+    const result = await salesModel.findById(2);
+
+    expect(result).to.be.deep.equal(findByIdMock);
+  })
+
   afterEach(sinon.restore);
 });
