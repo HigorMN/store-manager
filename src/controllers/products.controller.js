c526724e61af5300b28f52a993db616a9a2e9d30
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-01-16 12:09:52 -0300

    feat: update by id controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -24,8 +24,20 @@ const newProduct = async (req, res) => {
   return res.status(201).json(message);
 };
 
+const updateProductById = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const { type, message } = await productsService.updateById(name, id);
+
+  if (type) return res.status(404).json({ message });
+
+  return res.status(200).json(message);
+};
+
 module.exports = {
   listProducts,
   getProfuct,
   newProduct,
+  updateProductById,
 };
